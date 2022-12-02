55f0418e7995feff243a35a0fa70da5313c1bb71
DDL <78134447+nirvana20204@users.noreply.github.com>
2022-12-02 09:55:53 +0800

    更新element-ui引入

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import i18n from "./i18n";
+import ElementUI from 'element-ui';                      // 引入element-ui
+import 'element-ui/lib/theme-chalk/index.css';
 import "./styles/reset.scss";
 import "./styles/global.scss";
 import "./assets/local/iconfonts/iconfont.css";
@@ -12,7 +14,6 @@ import filters from "./utils/filters";
 Object.keys(filters).forEach((key) => {
   Vue.filter(key, filters[key]);
 });
-
 import api from "./api/api";
 Vue.prototype.$api = api;
 
@@ -20,54 +21,11 @@ import VueAwesomeSwiper from "vue-awesome-swiper";
 import "swiper/dist/css/swiper.css";
 Vue.use(VueAwesomeSwiper);
 
-import {
-  Icon,
-  Button,
-  Message,
-  Steps,
-  Step,
-  Dialog,
-  Collapse,
-  CollapseItem,
-  Tag,
-  Select,
-  Option,
-  Tooltip,
-  Backtop,
-  Drawer,
-  Menu,
-  Submenu,
-  MenuItem,
-  MenuItemGroup,
-  DatePicker,
-  Loading,
-  Popover
-} from "element-ui";
-Vue.use(Icon);
-Vue.use(Button);
-Vue.use(Steps);
-Vue.use(Step);
-Vue.use(Dialog);
-Vue.use(Collapse);
-Vue.use(CollapseItem);
-Vue.use(Tag);
-Vue.use(Select);
-Vue.use(Option);
-Vue.use(Tooltip);
-Vue.use(Backtop);
-Vue.use(Drawer);
-Vue.use(Menu);
-Vue.use(Submenu);
-Vue.use(MenuItem);
-Vue.use(MenuItemGroup);
-Vue.use(DatePicker);
-Vue.use(Popover);
+
+Vue.use(ElementUI); 
 Vue.prototype.$message = (option) => {
   option.customClass = i18n.locale == "en" ? "fontfamily_en" : "fontfamily_zh";
-  return Message(option);
 };
-Vue.use(Loading); // 使用服务方式的话，只安装Loading即可
-Vue.use(Loading.directive); //  指令方式(v-loading)的话这两行都得有
 
 Vue.config.productionTip = false;
 new Vue({
